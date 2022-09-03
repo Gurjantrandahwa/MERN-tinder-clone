3a576b724e43b6f73b07f1a43241476a9c4c0f5c
Gurjant Singh <gurjantrandhawa74456@gmail.com>
2022-09-03 19:46:10 +0530

    TinderCards implemented

---
+++ tinder-clone/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBNG8ktEPvT7bcwBGE8EtqJgcuaEklmdYM",
+    authDomain: "tinder-clone-ae967.firebaseapp.com",
+    projectId: "tinder-clone-ae967",
+    storageBucket: "tinder-clone-ae967.appspot.com",
+    messagingSenderId: "60951585452",
+    appId: "1:60951585452:web:5cbd360249b403f240f509",
+    measurementId: "G-Y9CYERSQ74"
+};
